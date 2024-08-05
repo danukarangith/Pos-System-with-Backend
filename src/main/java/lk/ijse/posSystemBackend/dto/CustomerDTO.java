8acd07f93da63321c4cb9b95a47b1e93325cc37c
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:40:43 +0530

    customer dto implement

--- src/main/java/lk/ijse/posSystemBackend/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/posSystemBackend/dto/CustomerDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.posSystemBackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CustomerDTO {
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
+
 }
