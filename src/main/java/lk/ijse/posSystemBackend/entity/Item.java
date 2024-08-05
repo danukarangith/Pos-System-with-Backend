d3ab3e504cc6daa451b71385f7ecf0d98a9f5320
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:46:23 +0530

    item entity implented

--- src/main/java/lk/ijse/posSystemBackend/entity/Item.java
+++ src/main/java/lk/ijse/posSystemBackend/entity/Item.java
@@ -1,4 +1,15 @@
 package lk.ijse.posSystemBackend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Item {
+    private String code;
+    private String description;
+    private double unitPrice;
+    private int qtyOnHand;
 }
