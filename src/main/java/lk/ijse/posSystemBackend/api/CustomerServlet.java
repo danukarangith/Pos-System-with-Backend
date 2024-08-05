60796bf332da641bf3ca46b2494f8050d626c00b
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:29:46 +0530

    url pattern add for CustomerServlet

--- src/main/java/lk/ijse/posSystemBackend/api/CustomerServlet.java
+++ src/main/java/lk/ijse/posSystemBackend/api/CustomerServlet.java
@@ -1,7 +1,10 @@
 package lk.ijse.posSystemBackend.api;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+
+@WebServlet(urlPatterns = "/Customers")
 public class CustomerServlet extends HttpServlet {
 
 
