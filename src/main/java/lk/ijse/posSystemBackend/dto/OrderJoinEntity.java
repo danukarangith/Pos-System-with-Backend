394f69f6646049cd906d4e06a84fbb48f58b75c6
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:43:58 +0530

    OrderJoinEntity implented

--- src/main/java/lk/ijse/posSystemBackend/dto/OrderJoinEntity.java
+++ src/main/java/lk/ijse/posSystemBackend/dto/OrderJoinEntity.java
@@ -1,4 +1,23 @@
 package lk.ijse.posSystemBackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class OrderJoinEntity {
+
+    private String orderID;
+    private LocalDate orderDate;
+    private String customerID;
+    private String itemCode;
+    private int itemQty;
+    private double unitPrice;
+
 }
