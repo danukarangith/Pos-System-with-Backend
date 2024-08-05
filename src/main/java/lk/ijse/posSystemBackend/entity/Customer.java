fbb873a5c8a2543e01581e57d5273fd7ea8045ae
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:45:45 +0530

    customer entity implented

--- src/main/java/lk/ijse/posSystemBackend/entity/Customer.java
+++ src/main/java/lk/ijse/posSystemBackend/entity/Customer.java
@@ -1,4 +1,17 @@
 package lk.ijse.posSystemBackend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
+
 }
