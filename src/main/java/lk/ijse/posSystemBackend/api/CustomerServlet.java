9b439989289cc27fe37d291ae49482aa54406541
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:34:17 +0530

    init mehod implemnt

--- src/main/java/lk/ijse/posSystemBackend/api/CustomerServlet.java
+++ src/main/java/lk/ijse/posSystemBackend/api/CustomerServlet.java
@@ -3,9 +3,20 @@ package lk.ijse.posSystemBackend.api;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 
 @WebServlet(urlPatterns = "/Customers")
 public class CustomerServlet extends HttpServlet {
+    private DataSource source;
 
-
+public void init() {
+    try {
+        source=(DataSource) new InitialContext().lookup("java:/comp/env/jdbc/pos");
+    } catch (NamingException e) {
+        throw new RuntimeException(e);
+    }
+}
 }
