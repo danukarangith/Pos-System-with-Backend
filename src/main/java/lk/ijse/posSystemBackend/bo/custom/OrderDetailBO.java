9e5d7705307e8bec76ad5c2f41e8f3be68281ed6
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:58:00 +0530

    orderdetailbo implemented

--- src/main/java/lk/ijse/posSystemBackend/bo/custom/OrderDetailBO.java
+++ src/main/java/lk/ijse/posSystemBackend/bo/custom/OrderDetailBO.java
@@ -1,4 +1,12 @@
 package lk.ijse.posSystemBackend.bo.custom;
 
-public interface OrderDetailBO {
+import lk.ijse.posSystemBackend.bo.SuperBO;
+import lk.ijse.posSystemBackend.dto.OrderDetailDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface OrderDetailBO extends SuperBO {
+    ArrayList<OrderDetailDTO> getAllDetails(Connection connection) throws SQLException, ClassNotFoundException;
 }
