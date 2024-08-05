2f13cda4a6ea2357a911b09713df164e2a180c5d
danukarangith <danukarangith456@gmail.com>
2024-08-06 01:00:16 +0530

    purchaseprderbo implemented

--- src/main/java/lk/ijse/posSystemBackend/bo/custom/PurchaseOrderBO.java
+++ src/main/java/lk/ijse/posSystemBackend/bo/custom/PurchaseOrderBO.java
@@ -1,4 +1,11 @@
 package lk.ijse.posSystemBackend.bo.custom;
 
-public class PurchaseOrderBO {
+import lk.ijse.posSystemBackend.bo.SuperBO;
+import lk.ijse.posSystemBackend.dto.OrderDTO;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public interface PurchaseOrderBO extends SuperBO {
+    boolean saveOrder(OrderDTO dto, DataSource source) throws SQLException, ClassNotFoundException;
 }
