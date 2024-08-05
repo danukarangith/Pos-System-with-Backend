d3dada7c2d587354fd1178ba7934c983a271d0e9
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:46:58 +0530

    orderdetail entity implented

--- src/main/java/lk/ijse/posSystemBackend/entity/OrderDetails.java
+++ src/main/java/lk/ijse/posSystemBackend/entity/OrderDetails.java
@@ -1,4 +1,17 @@
 package lk.ijse.posSystemBackend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class OrderDetails {
+
+    private String orderId;
+    private String itemCode;
+    private int qty;
+    private double unitPrice;
+
 }
