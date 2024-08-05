2263b91274c619da708d1054025731fbf66635d5
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:42:31 +0530

    orderdetail implented

--- src/main/java/lk/ijse/posSystemBackend/dto/OrderDetailDTO.java
+++ src/main/java/lk/ijse/posSystemBackend/dto/OrderDetailDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.posSystemBackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class OrderDetailDTO {
+    private String orderId;
+    private String itemCode;
+    private int qty;
+    private double unitPrice;
 }
