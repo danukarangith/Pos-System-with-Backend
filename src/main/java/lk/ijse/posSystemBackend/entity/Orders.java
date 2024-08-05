ede532556c30cbc84e693e071f326793a81350c6
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:47:47 +0530

    order entity implented

--- src/main/java/lk/ijse/posSystemBackend/entity/Orders.java
+++ src/main/java/lk/ijse/posSystemBackend/entity/Orders.java
@@ -1,4 +1,17 @@
 package lk.ijse.posSystemBackend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Orders {
+    private String id;
+    private LocalDate date;
+    private String  customerID;
+
 }
