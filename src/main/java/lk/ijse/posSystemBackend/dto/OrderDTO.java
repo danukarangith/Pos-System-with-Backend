f49ae477ddb20c7be13bb3262e344af5f77dc045
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:43:13 +0530

    orderdto implented

--- src/main/java/lk/ijse/posSystemBackend/dto/OrderDTO.java
+++ src/main/java/lk/ijse/posSystemBackend/dto/OrderDTO.java
@@ -1,4 +1,21 @@
 package lk.ijse.posSystemBackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class OrderDTO {
+    private String id;
+    private LocalDate date;
+    private String customerId;
+    List<OrderDetailDTO> orderDetaisList;
+
 }
