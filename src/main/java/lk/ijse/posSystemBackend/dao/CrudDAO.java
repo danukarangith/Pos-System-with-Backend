41618f08ef49e9a6d7a5d26972b1c092924841a2
danukarangith <danukarangith456@gmail.com>
2024-08-06 01:02:41 +0530

    crudDao implemented

--- src/main/java/lk/ijse/posSystemBackend/dao/CrudDAO.java
+++ src/main/java/lk/ijse/posSystemBackend/dao/CrudDAO.java
@@ -1,4 +1,19 @@
 package lk.ijse.posSystemBackend.dao;
 
-public interface CrudDAO {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDAO<T,ID> extends SuperDAO{
+    ArrayList<T> getAll(Connection connection) throws SQLException, ClassNotFoundException;
+
+    boolean save(T dto,Connection connection) throws SQLException, ClassNotFoundException;
+
+    boolean update(T dto,Connection connection) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id,Connection connection) throws SQLException, ClassNotFoundException;
+
+    boolean exist(ID id,Connection connection) throws SQLException, ClassNotFoundException;
+
+    T search(ID code, Connection connection)throws SQLException, ClassNotFoundException;
 }
