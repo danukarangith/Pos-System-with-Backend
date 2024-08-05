2ac4b00b8338122c941fa59191cddb13f442308a
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:55:28 +0530

    itembo implemented

--- src/main/java/lk/ijse/posSystemBackend/bo/custom/ItemBO.java
+++ src/main/java/lk/ijse/posSystemBackend/bo/custom/ItemBO.java
@@ -1,4 +1,18 @@
 package lk.ijse.posSystemBackend.bo.custom;
 
-public interface ItemBO {
+import lk.ijse.posSystemBackend.bo.SuperBO;
+import lk.ijse.posSystemBackend.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface ItemBO extends SuperBO {
+    ArrayList<ItemDTO> getAllItems(Connection connection) throws SQLException, ClassNotFoundException ;
+
+    boolean deleteItem(String code ,Connection connection) throws SQLException, ClassNotFoundException ;
+
+    boolean saveItem(ItemDTO dto,Connection connection) throws SQLException, ClassNotFoundException ;
+
+    boolean updateItem(ItemDTO dto,Connection connection) throws SQLException, ClassNotFoundException;
 }
