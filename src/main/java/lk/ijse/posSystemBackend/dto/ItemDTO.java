f8cff3cf9a78e179c223cce6f8c0dfc7e89fce45
danukarangith <danukarangith456@gmail.com>
2024-08-06 00:41:37 +0530

    ItemDTO implented

--- src/main/java/lk/ijse/posSystemBackend/dto/ItemDTO.java
+++ src/main/java/lk/ijse/posSystemBackend/dto/ItemDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.posSystemBackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ItemDTO {
+    private String code;
+    private String description;
+    private double unitPrice;
+    private int qtyOnHand;
 }
