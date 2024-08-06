2a712140811fc3381c1277e795e5f8ab561f28cc
danukarangith <danukarangith456@gmail.com>
2024-08-06 12:46:55 +0530

    ItemDAOImpl crud implemnt

--- src/main/java/lk/ijse/posSystemBackend/dao/custom/impl/ItemDAOImpl.java
+++ src/main/java/lk/ijse/posSystemBackend/dao/custom/impl/ItemDAOImpl.java
@@ -19,4 +19,34 @@ public class ItemDAOImpl  implements ItemDAO {
         }
         return allItems;
     }
+    @Override
+    public boolean save(Item entity,Connection connection) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute(connection,"INSERT INTO item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)", entity.getCode(), entity.getDescription(), entity.getUnitPrice(), entity.getQtyOnHand());
+
+    }
+
+    @Override
+    public boolean update(Item entity,Connection connection) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute(connection,"UPDATE item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?", entity.getDescription(), entity.getUnitPrice(), entity.getQtyOnHand(), entity.getCode());
+    }
+
+    @Override
+    public boolean delete(String code,Connection connection) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute(connection,"DELETE FROM item WHERE code=?", code);
+    }
+
+    @Override
+    public boolean exist(String s, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Item search(String code, Connection connection) throws SQLException, ClassNotFoundException {
+        ResultSet rst = SQLUtil.execute(connection,"SELECT * FROM item WHERE code=?", code);
+        if (rst.next()) {
+            return new Item(rst.getString(1), rst.getString(2),  rst.getDouble(3),rst.getInt(4));
+        }
+        return null;
+    }
+
 }
