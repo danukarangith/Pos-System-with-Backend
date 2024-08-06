9f78f0700fed77be5258a74d8684a5f2cb6914e9
danukarangith <danukarangith456@gmail.com>
2024-08-07 00:16:52 +0530

    PurchaseOrderServlet init implement

--- src/main/java/lk/ijse/posSystemBackend/api/PurchaseOrderServlet.java
+++ src/main/java/lk/ijse/posSystemBackend/api/PurchaseOrderServlet.java
@@ -1,4 +1,28 @@
 package lk.ijse.posSystemBackend.api;
 
-public class PurchaseOrderServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import lk.ijse.posSystemBackend.bo.BoFactory;
+import lk.ijse.posSystemBackend.bo.custom.PurchaseOrderBO;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+@WebServlet(urlPatterns = "/orders")
+public class PurchaseOrderServlet extends HttpServlet {
+
+    PurchaseOrderBO poBO = BoFactory.getBoFactory().getBO(BoFactory.BOTypes.PURCHASE_ORDER_BO);
+
+    private DataSource source;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            source = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/pos");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
 }
