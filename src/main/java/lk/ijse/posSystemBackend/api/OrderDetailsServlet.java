9cfd3b864d43425aa77da6b8fcbd0e5013df80f9
danukarangith <danukarangith456@gmail.com>
2024-08-07 00:12:56 +0530

    orderdetail init method implement

--- src/main/java/lk/ijse/posSystemBackend/api/OrderDetailsServlet.java
+++ src/main/java/lk/ijse/posSystemBackend/api/OrderDetailsServlet.java
@@ -1,4 +1,25 @@
 package lk.ijse.posSystemBackend.api;
 
-public class OrderDetailsServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import lk.ijse.posSystemBackend.bo.BoFactory;
+import lk.ijse.posSystemBackend.bo.custom.OrderDetailBO;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class OrderDetailsServlet extends HttpServlet {
+    OrderDetailBO detailBO = BoFactory.getBoFactory().getBO(BoFactory.BOTypes.Detail_BO);
+
+    private DataSource source;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            source = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/pos");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
 }
