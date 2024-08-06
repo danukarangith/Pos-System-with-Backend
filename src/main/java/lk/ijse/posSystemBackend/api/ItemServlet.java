017db981dd73966e4fb8bd2e395256177bcbb7d6
danukarangith <danukarangith456@gmail.com>
2024-08-06 14:07:38 +0530

    item doGet implement

--- src/main/java/lk/ijse/posSystemBackend/api/ItemServlet.java
+++ src/main/java/lk/ijse/posSystemBackend/api/ItemServlet.java
@@ -1,17 +1,27 @@
 package lk.ijse.posSystemBackend.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.posSystemBackend.bo.BoFactory;
 import lk.ijse.posSystemBackend.bo.custom.ItemBO;
+import lk.ijse.posSystemBackend.dto.ItemDTO;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 @WebServlet(urlPatterns = "/items")
 public class ItemServlet extends HttpServlet {
+
     ItemBO itemBO =  BoFactory.getBoFactory().getBO(BoFactory.BOTypes.ITEM_BO);
 
     private DataSource source;
@@ -22,6 +32,17 @@ public class ItemServlet extends HttpServlet {
             source = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/pos");
         } catch (NamingException e) {
             throw new RuntimeException(e);
+        }
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try(Connection connection = source.getConnection();) {
+            ArrayList<ItemDTO> allitems = itemBO.getAllItems(connection);
+            resp.setContentType("application/json");
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(allitems,resp.getWriter());
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
         }
     }
 }
