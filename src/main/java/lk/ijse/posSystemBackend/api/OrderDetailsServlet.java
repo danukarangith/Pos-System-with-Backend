92dfba56b736a8cb5c891c0e38faab1814b304ad
danukarangith <danukarangith456@gmail.com>
2024-08-07 00:13:33 +0530

    orderdetail doget implement

--- src/main/java/lk/ijse/posSystemBackend/api/OrderDetailsServlet.java
+++ src/main/java/lk/ijse/posSystemBackend/api/OrderDetailsServlet.java
@@ -1,13 +1,22 @@
 package lk.ijse.posSystemBackend.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.posSystemBackend.bo.BoFactory;
 import lk.ijse.posSystemBackend.bo.custom.OrderDetailBO;
+import lk.ijse.posSystemBackend.dto.OrderDetailDTO;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class OrderDetailsServlet extends HttpServlet {
     OrderDetailBO detailBO = BoFactory.getBoFactory().getBO(BoFactory.BOTypes.Detail_BO);
@@ -20,6 +29,17 @@ public class OrderDetailsServlet extends HttpServlet {
             source = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/pos");
         } catch (NamingException e) {
             e.printStackTrace();
+        }
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = source.getConnection();){
+            ArrayList<OrderDetailDTO> alldetails = detailBO.getAllDetails(connection);
+            resp.setContentType("application/json");
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(alldetails,resp.getWriter());
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
         }
     }
 }
