87a790d9213602b5fb87cdc901417123d709087e
danukarangith <danukarangith456@gmail.com>
2024-08-06 12:46:08 +0530

    item getall implemnt

--- src/main/java/lk/ijse/posSystemBackend/dao/custom/impl/ItemDAOImpl.java
+++ src/main/java/lk/ijse/posSystemBackend/dao/custom/impl/ItemDAOImpl.java
@@ -1,4 +1,22 @@
 package lk.ijse.posSystemBackend.dao.custom.impl;
 
-public class ItemDAOImpl {
+import lk.ijse.posSystemBackend.dao.custom.ItemDAO;
+import lk.ijse.posSystemBackend.dao.custom.impl.util.SQLUtil;
+import lk.ijse.posSystemBackend.entity.Item;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemDAOImpl  implements ItemDAO {
+    @Override
+    public ArrayList<Item> getAll(Connection connection) throws SQLException, ClassNotFoundException {
+        ResultSet rst = SQLUtil.execute(connection,"SELECT * FROM item");
+        ArrayList<Item> allItems = new ArrayList<>();
+        while (rst.next()) {
+            allItems.add(new Item(rst.getString(1), rst.getString(2), rst.getDouble(3),rst.getInt(4)));
+        }
+        return allItems;
+    }
 }
