f164cd0f30fcba26b02b038b72ffea86e0a5e0b2
danukarangith <danukarangith456@gmail.com>
2024-08-06 14:06:42 +0530

    item servlet init method implement

--- src/main/java/lk/ijse/posSystemBackend/api/ItemServlet.java
+++ src/main/java/lk/ijse/posSystemBackend/api/ItemServlet.java
@@ -1,8 +1,27 @@
 package lk.ijse.posSystemBackend.api;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import lk.ijse.posSystemBackend.bo.BoFactory;
+import lk.ijse.posSystemBackend.bo.custom.ItemBO;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 @WebServlet(urlPatterns = "/items")
 public class ItemServlet extends HttpServlet {
+    ItemBO itemBO =  BoFactory.getBoFactory().getBO(BoFactory.BOTypes.ITEM_BO);
+
+    private DataSource source;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            source = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/pos");
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
