7f372938b887c603a2b6ec93fb77f4bcf3af34d9
danukarangith <danukarangith456@gmail.com>
2024-08-06 12:43:17 +0530

    customer getall implement

--- src/main/java/lk/ijse/posSystemBackend/dao/custom/impl/CustomerDAOImpl.java
+++ src/main/java/lk/ijse/posSystemBackend/dao/custom/impl/CustomerDAOImpl.java
@@ -1,6 +1,22 @@
 package lk.ijse.posSystemBackend.dao.custom.impl;
 
 import lk.ijse.posSystemBackend.dao.custom.CustomerDAO;
+import lk.ijse.posSystemBackend.dao.custom.impl.util.SQLUtil;
+import lk.ijse.posSystemBackend.entity.Customer;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CustomerDAOImpl implements CustomerDAO {
+    @Override
+    public ArrayList<Customer> getAll(Connection connection) throws SQLException, ClassNotFoundException {
+        ArrayList<Customer> allCustomers = new ArrayList<>();
+        ResultSet rst = SQLUtil.execute(connection,"SELECT * FROM customer");
+        while (rst.next()) {
+            allCustomers.add(new Customer(rst.getString(1), rst.getString(2), rst.getString(3),rst.getDouble(4)));
+        }
+        return allCustomers;
+    }
 }
